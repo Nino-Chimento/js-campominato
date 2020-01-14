6d8d670d09e3729fbae321a0217414c749ef23fc
Nino Chimento <saninoc84@gmail.com>
2020-01-14 12:43:58 +0100

    variabile utente con autenticazioen numeri compresi da 1 a 100

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 // In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+var punteggio = 0;
+numeroUtente = parseInt(prompt("inserisci un numero comrpeso da 1 a 100"))
+while (numeroUtente > 1 || numeroUtente < 100) {
+  numeroUtente = parseInt(prompt("inserisci un numero comrpeso da 1 a 100"))
+}
+// creazione nuermi random
 var listaNumeriPc =[];
 for (var i = 0; i <16; i++) {
   numeroCreato = getRandomInt(1,100);
