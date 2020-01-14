665fe664ec7a1b4e027751bddc891bd14beaa1b3
Nino Chimento <saninoc84@gmail.com>
2020-01-14 13:12:24 +0100

    sistemazione

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Il computer deve generare 16 numeri casuali da 1 a 100.
-// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-var punteggio = 0;
 
+var punteggio = 0;
+var controllo;
 numeroUtente = parseInt(prompt("inserisci un numero comrpeso da 1 a 100"));
 while (numeroUtente < 1 || numeroUtente > 100) {
   numeroUtente = parseInt(prompt("inserisci un numero comrpeso da 1 a 100"))
@@ -30,5 +27,3 @@ function comparazione(lista,numero) {
     }
   }
 }
-controllo = comparazione(listaNumeriPc,numeroUtente);
-console.log(controllo);
