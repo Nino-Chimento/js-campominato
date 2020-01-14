ea82c2456622630009b024961407f1b32d0de28a
Nino Chimento <saninoc84@gmail.com>
2020-01-14 12:40:44 +0100

    creazione array con 16 numeri casuali

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("n")
+// Il computer deve generare 16 numeri casuali da 1 a 100.
+// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+var listaNumeriPc =[];
+for (var i = 0; i <16; i++) {
+  numeroCreato = getRandomInt(1,100);
+  listaNumeriPc.push(numeroCreato)
+  }
+  console.log(listaNumeriPc);
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
