53fbcb4f8b4b0320524239030bfbcf298b89581d
Nino Chimento <saninoc84@gmail.com>
2020-01-14 16:12:47 +0100

    creazione difficolta livello 1

--- script.js
+++ script.js
@@ -19,10 +19,35 @@ if (difficolta == 0) {
         controllo = comparazione(listaNumeriPc,numeroUtente);
         console.log(controllo);
         if (controllo == true) {
-          alert("hai perso" + punteggio);
+          alert("hai perso il tuo punteggio e'"+" " + punteggio);
           i = 84
         }
         else if (punteggio == 84) {
+          alert("secondo me hai barato ma hai vinto!!!")
+        }
+        else {
+          punteggio++
+        }
+      }
+}
+else if (difficolta == 1) {
+  for (var i = 0; i <16; i++) {
+    numeroCreato = getRandomInt(1,80);
+    listaNumeriPc.push(numeroCreato)
+    }
+    console.log(listaNumeriPc);
+    for (var i = 0; i < 64; i++) {
+        numeroUtente = parseInt(prompt("inserisci un numero comrpeso da 1 a 80"));
+        while (numeroUtente < 1 || numeroUtente > 80) {
+          numeroUtente = parseInt(prompt("devi inserire un numero da 1 a 80"))
+        }
+        controllo = comparazione(listaNumeriPc,numeroUtente);
+        console.log(controllo);
+        if (controllo == true) {
+          alert("hai perso il tuo punteggio e'"+" " + punteggio);
+          i = 64
+        }
+        else if (punteggio == 64) {
           alert("secondo me hai barato ma hai vinto!!!")
         }
         else {
