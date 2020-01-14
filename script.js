682fac9c2117e6dcb841d0cc846e196756868740
Nino Chimento <saninoc84@gmail.com>
2020-01-14 12:54:25 +0100

    funzione di comparazione

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 var punteggio = 0;
 numeroUtente = parseInt(prompt("inserisci un numero comrpeso da 1 a 100"))
-while (numeroUtente > 1 || numeroUtente < 100) {
+while (numeroUtente < 1 || numeroUtente > 100) {
   numeroUtente = parseInt(prompt("inserisci un numero comrpeso da 1 a 100"))
 }
 // creazione nuermi random
-var listaNumeriPc =[];
+var listaNumeriPc =[23,];
 for (var i = 0; i <16; i++) {
   numeroCreato = getRandomInt(1,100);
   listaNumeriPc.push(numeroCreato)
@@ -18,4 +18,11 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
+}
+function comparazione(lista,numero) {
+  for (var i = 0; i < lista.length; i++) {
+    if (lista[i]==numero) {
+      console.log("nino");
+    }
+  }
 }
