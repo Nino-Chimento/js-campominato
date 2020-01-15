ac1b2ae0227563a563b99fb0160db4c9ee863bb5
Nino Chimento <saninoc84@gmail.com>
2020-01-15 12:56:04 +0100

    creazione funzioni numeri random e controllo presenza nella lista

--- script2.js
+++ script2.js
@@ -1,1 +1,43 @@
-alert("n")
+var listaBomb=[23,]
+var listaNumeriUtente=[];
+var punteggio = 0;
+var tentativi = 5;
+var chek = false;
+
+
+for (var i = 0; i < 8; i++) {
+  listaBomb.push(getRandomInt(1,100))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// funzioni
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+function chek(array,valore){
+  for (var i = 0; i < array.length; i++) {
+    if (valore == array[i]) {
+        return true;
+    }
+  }
+}
